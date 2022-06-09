0a57ff079ad0d9e79d3c607ebe62cc7d85b52fdf
snovamicron <mdip5675@gmail.com>
2022-06-09 13:33:01 +0530

    need to complete the css

--- client/src/components/Product/ProductDetails.js
+++ client/src/components/Product/ProductDetails.js
@@ -4,6 +4,10 @@ import { Carousel } from 'react-responsive-carousel';
 import "./ProductDetails.css"
 import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router-dom";
+import ReactStars from "react-rating-stars-component"
+
+// components
+import Loader from "../layout/Loader/Loader.js"
 
 // actions
 import { getProductDetails } from "../../Actions/ProductAction"
@@ -14,20 +18,25 @@ const ProductDetails = () => {
     const { load, error, product } = useSelector(state => state.productDetails) 
     useEffect(() => {
         dispatch(getProductDetails(id))
+        window.scrollTo(0,0)
     }, [dispatch, id])
     return (
         <>
+        {
+            load? 
+            <Loader/> 
+            :
+            <>
         <div className="productDetails">
             <div>
             <Carousel
-            width={"50%"}
-            useKeyboardArrows={true}
-            centerMode={true}
-            centerSlidePercentage={100}
+            width="25vmax"
             showArrows={false}
-            thumbWidth={"4vmax"}
             showIndicators={false}
-            swipeable={true}
+            thumbWidth="5vmax"
+            autoPlay={true}
+            infiniteLoop={true}
+            interval={2000}
             >
                 {
                     product.images && 
@@ -41,7 +50,40 @@ const ProductDetails = () => {
                 }
             </Carousel>
             </div>
+            <div>
+                <div className="detailsBlock-1">
+                    <h2>{product.name}</h2>
+                    <p>Product # {product._id}</p>
+                </div>
+                <div className="detailsBlock-2">
+                    <ReactStars/>
+                    <span>( {product.numOfReviews} Reviews )</span>
+                </div>
+                <div className="detailsBlock-3">
+                    <h1>₹{product.price}</h1>
+                    <div className="detailsBlock-3-1">
+                        <div className="detailsBlock-3-1-1">
+                            <button>-</button>
+                            <input type="number" value={1} />
+                            <button>+</button>
+                        </div>
+                        <button>Add to Cart</button>
+                    </div>
+                    <p>
+                        Status: &ensp;
+                        <b className={product.stock < 1? "redColor" : "greenColor"}>
+                            {product.stock < 1? "Out of Stock" : "In Stock"}
+                        </b>
+                    </p>
+                </div>
+                <div className="detailsBlock-4">
+                Description: <p>{product.description}</p>
+                    <button className="submitReview">Submit Review</button>
+                </div>
+            </div>
         </div>
+        </>
+        }
         </>
     )
 }
