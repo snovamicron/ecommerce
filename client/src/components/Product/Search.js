4ecf42d0f2f380695ae13a97464e0d59f4b1eccd
snovamicron <mdip5675@gmail.com>
2022-06-24 11:44:29 +0530

    need to work on history.push and all

--- client/src/components/Product/Search.js
+++ client/src/components/Product/Search.js
@@ -1,12 +1,28 @@
-
-
+import { useState } from "react"
+import "./Search.css"
+ 
 
 const Search = ({ history }) => {
+    const [keyWord, setKeyWord] = useState("")
+    const searchSubmitHandler = (e) => {
+        e.preventDefault()
+        console.log(keyWord);
+        if(keyWord.trim()){
+            history.push(`/products/${keyWord}`)
+        }else{
+            history.push("/products")
+        }
+    }
     return (
         <>
-        <from className="searchBox">
-            
-        </from>
+        <form className="searchBox" onSubmit={(e) => searchSubmitHandler(e)} >
+            <input 
+            type="text"
+            placeholder="Search a product...." 
+            onChange={(e) => setKeyWord(e.target.value)}
+            />
+            <input type="submit" value="search"/>
+        </form>
         </>
     )
 }
